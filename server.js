cc8a502d58553dca282e94c82f9f9f5c55108137
balvesD3v <lpbalves7filho@gmail.com>
2024-06-11 20:50:39 -0300

    websocket

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ wss.on("connection", (ws, req) => {
   console.log(`New client connected: user_id=${userId}, role=${role}`);
 
   ws.on("message", (message) => {
-    console.log(`Message received from user_id=${userId}: ${message}`);
+    console.log(`Received message from user_id=${userId}: ${message}`);
+
     const parsedMessage = JSON.parse(message);
 
     if (parsedMessage.type === "start_chat") {
@@ -29,18 +30,14 @@ wss.on("connection", (ws, req) => {
           client.userId === parsedMessage.consultant_id &&
           client.role === "consultant"
         ) {
-          const newChatMessage = {
-            type: "new_chat",
-            chat_session_id: parsedMessage.chat_session_id,
-            user_id: userId,
-            user_name: parsedMessage.user_name,
-          };
-          console.log(
-            `Sending new_chat to consultant_id=${
-              parsedMessage.consultant_id
-            }: ${JSON.stringify(newChatMessage)}`
+          client.send(
+            JSON.stringify({
+              type: "new_chat",
+              chat_session_id: parsedMessage.chat_session_id,
+              user_id: userId,
+              user_name: parsedMessage.user_name,
+            })
           );
-          client.send(JSON.stringify(newChatMessage));
         }
       });
     } else if (parsedMessage.type === "chat") {
@@ -53,9 +50,6 @@ wss.on("connection", (ws, req) => {
           client.readyState === WebSocket.OPEN &&
           (client.userId === parsedMessage.to || client.userId === userId)
         ) {
-          console.log(
-            `Sending chat message to client with user_id=${client.userId}: ${message}`
-          );
           client.send(message);
         }
       });
